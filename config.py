7f62d0592ae482fd2db0ee2392af6b89b8e17695
lucamignozzi <luca.mignozzi@skywarder.eu>
2021-03-13 12:17:22 +0100

    add config HermesX

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ from pythonCode.meshClasses.point import point
 import pythonCode.meshClasses.dynamicMesh as dynamicMesh
 
 #simulation batch name
-simulationName = "simulazione0.7"
+simulationName = "simulazione0.3"
 
 # --------------------------------------------------------------------------------------------
 # MESHING PART
@@ -29,14 +29,17 @@ cfmeshConfiguration.maxFirstlayerThickness = 0.00001 #maximum allowable thicknes
 
 cfmeshConfiguration.refinementZones = [
 	# cfmeshRefinement(NAME, CELLSIZE, POINT1, POINT2, RADIUS1, RADIUS2) ==> create a refinement zone shaped like a cone
-	cfmesh.cfmeshRefinement("wake_superfine", 0.002, point(0.98, 0, 0), point(1.2, 0, 0), 0.05, 0.0692),
-	cfmesh.cfmeshRefinement("wake_fine", 0.03, point(-1.4, 0, 0), point(2.5, 0, 0), 0.02, 0.541),
-	cfmesh.cfmeshRefinement("wake_medium", 0.06, point(2.5, 0, 0), point(5, 0, 0), 0.76, 1.02),
-	cfmesh.cfmeshRefinement("wake_coarse", 0.09, point(5, 0, 0), point(8, 0, 0), 0.76, 1.02),
 	cfmesh.cfmeshRefinement("ogive_far", 0.06, point(-1.5, 0, 0), point(0.2, 0, 0), 0.7, 0.848),
 	cfmesh.cfmeshRefinement("ogive_area", 0.01, point(-0.6, 0, 0), point(-1.5, 0, 0), 0.15, 0.15),
 	cfmesh.cfmeshRefinement("ogive_narrow", 0.005, point(-1.02, 0, 0), point(-0.6, 0, 0), 0.03, 0.07),
-	cfmesh.cfmeshRefinement("winglet_zone", 0.01, point(0.8, 0, 0), point(1.5, 0, 0), 0.15, 0.21)
+
+	cfmesh.cfmeshRefinement("winglet_zone", 0.01, point(0.8, 0, 0), point(1.5, 0, 0), 0.15, 0.21),
+
+	cfmesh.cfmeshRefinement("wake_superfine", 0.002, point(0.98, 0, 0), point(1.2, 0, 0), 0.05, 0.0692),
+	cfmesh.cfmeshRefinement("wake_fine", 0.03, point(-1.4, 0, 0), point(2.5, 0, 0), 0.02, 0.541),
+	cfmesh.cfmeshRefinement("wake_medium", 0.06, point(2.5, 0, 0), point(5, 0, 0), 0.76, 1.02),
+	cfmesh.cfmeshRefinement("wake_coarse", 0.09, point(5, 0, 0), point(8, 0, 0), 0.76, 1.02)
+
 ]
 
 #-------------------------------------------------SNAPPY HEX MESH CONFIGURATION-----------------------------------------------------------------------------------------------------
@@ -46,18 +49,22 @@ snappyConfiguration.rocketRefineLevel = snappy.refinementLevel(7,7) #rocket feat
 snappyConfiguration.boundaryLayers = 10  #define number of boundary layers
 
 snappyConfiguration.boundaryControl = snappy.boundaryControl() #add a boundary control
-snappyConfiguration.boundaryControl.setFirstLayerThickness(2.0e-5)
-snappyConfiguration.boundaryControl.setTotalThickness(1.5e-3)
+snappyConfiguration.boundaryControl.setFirstLayerThickness(1.0e-5)
+snappyConfiguration.boundaryControl.setTotalThickness(2.5e-3)
 
 snappyConfiguration.refinementZones = [
-	snappy.snappyRefinement("wake_coarse", snappy.refinementLevel(2,3), point(-1.4, 0, 0), point(8,0,0), 0.4),
-	snappy.snappyRefinement("ogive", snappy.refinementLevel(4,5), point(-1.1, 0, 0), point(-0.55,0,0), 0.15),
+	snappy.snappyRefinement("ogive",        snappy.refinementLevel(4,5), point(-0.15, 0, 0), point(0.40, 0, 0), 0.20),
 
-	snappy.snappyRefinement("winglet",     snappy.refinementLevel(6,7), point(0.85, 0, 0), point(1.05,0,0), 0.15),
+#	snappy.snappyRefinement("aerobreaks_1", snappy.refinementLevel(6,7), point( 1.65, 0, 0), point(2.45, 0, 0), 0.15),
+#	snappy.snappyRefinement("aerobreaks_2", snappy.refinementLevel(5,6), point( 1.55, 0, 0), point(2.45, 0, 0), 0.30),
 
-	snappy.snappyRefinement("wake_near_1", snappy.refinementLevel(6,7), point(1.0, 0, 0), point(1.2,0,0), 0.075),
-	snappy.snappyRefinement("wake_near_2", snappy.refinementLevel(5,6), point(0.95, 0, 0), point(1.5,0,0), 0.1),
-	snappy.snappyRefinement("wake_near_3", snappy.refinementLevel(4,5), point(0.8, 0, 0), point(2.5,0,0), 0.15)
+	snappy.snappyRefinement("winglet",      snappy.refinementLevel(6,7), point( 2.10, 0, 0), point(2.55, 0, 0), 0.20),
+
+	snappy.snappyRefinement("wake_near_1",  snappy.refinementLevel(6,7), point( 2.45, 0, 0), point(2.80, 0, 0), 0.10),
+	snappy.snappyRefinement("wake_near_2",  snappy.refinementLevel(5,6), point( 2.45, 0, 0), point(3.10, 0, 0), 0.15),
+	snappy.snappyRefinement("wake_near_3",  snappy.refinementLevel(4,5), point( 2.45, 0, 0), point(3.60, 0, 0), 0.20),
+
+	snappy.snappyRefinement("wake_coarse",  snappy.refinementLevel(2,3), point( 2.45, 0, 0), point(12.0, 0, 0), 0.50)
 ]
 # -------------------------------------------------------------------------------
 
